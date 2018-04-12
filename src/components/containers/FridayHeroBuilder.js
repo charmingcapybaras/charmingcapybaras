3d9142c6866fac4eff04ea23b1a49e9fe529e5f8
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-11 20:45:01 -0500

    Moved documentation into a Documentation folder

---
+++ src/components/containers/FridayHeroBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FridayHeroBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <p>FridayHero!</p>
+      </div>
+    );
+  }
+}
