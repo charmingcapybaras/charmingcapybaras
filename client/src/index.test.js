ff5ef5756a4158065e51383f3dd5dbff3592de0b
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-16 16:06:01 -0500

    Added the Jest testing plugins

---
+++ client/src/index.test.js
@@ -0,0 +1,5 @@
+const pass = true;
+
+test('Nonsensical passing test', () => {
+  expect(pass).toBe(true);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,22 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-   entry: `${SRC_DIR}/index.jsx`,
-   output: {
-     path: DIST_DIR,
-     filename: 'bundle.js'
-   }, 
-   module:{
-     rules: [{
+  entry: `${SRC_DIR}/index.jsx`,
+  mode: 'development',
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
-      }]
-   }
+      }
+    ]
+  }
 };
