c49b5f862ae9d616031ebdb956b5ee523a371169
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-26 13:29:28 -0500

    Removed one test

--- client/src/components/UserAgenda/UserAgenda.test.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import ReactDom from 'react-dom';
-// import UserAgenda from './UserAgenda';
-
-// test('UserAgenda mounts', () => {
-//   const div = document.createElement('div');
-//   ReactDom.render(<UserAgenda />, div);
-// });
